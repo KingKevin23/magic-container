8fc692fe45e68f2a9763ec2e9576d56f29c4fdb8
Kevin Baier <kevin.baier@protonmail.com>
2021-11-10 23:27:38 +0100

    changed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="magic-container",
-    version="0.0.1",
+    version="0.0.2",
     author="KingKevin23",
     author_email="code@kingkevin.de",
     description="Thread-safe, magical and persistent dict for Python.",
